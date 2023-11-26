7d5c9efb4876940b8d88be48817f098b89d96c74
andrettaj <andrettaj@aol.com>
2023-11-26 14:20:07 -0800

    adding and files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 const apiKey = b24ce2176971aa258bbd698656f9eba3
+
+const input = document.getElementById("search-input");
+const button = document.getElementById("search-input");
+
+
+
+button.addEventListener("click",function(){
+ console.log ("Search Button Clicked", input.value)
+
+});
+
+<input name="searchTxt" type="text" maxlength="512" id="searchTxt" class="searchField"/>
