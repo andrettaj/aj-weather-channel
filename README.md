f3941e4f243d40bb1ff718b0b1cb43ff4d64286b
andrettaj <andrettaj@aol.com>
2023-11-29 13:59:33 -0800

    updating code and adding readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # aj-weather-channel
+
+This project will retrieve weather data for cities.
+
+This project allows developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own.  Creates a learning tool for new developers like myself.
+
+This is a work in progress due to not finished but I am learning how to use an api key and some extra development tools. 
+
+What makes this project stand out is that you can enter any city and get the weather from it.  
+
+I had help from tudors and classmates and I grabbed alot of the code from class activities. 
+
+My github link is https://github.com/andrettaj/aj-weather-channel
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
-const apiKey = b24ce2176971aa258bbd698656f9eba3
-
+const apiKey = 19156974436f94b9edbe37a7f77ef6b1
 const input = document.getElementById("search-input");
 const button = document.getElementById("search-input");
 
+function getGeolocation(city,){
+    const geolocationURL = 'https://api.openweathermap.org/geo/1.0/direct?q=${city},&appid=${apiKey}';
+    fetch(geolcationUrl)
+    .then(function(res){
+     console.log(res)
+     return res.json()
+    })
+    .then(function(data){
+      console.log(data)  
+    })
+}
 
 
 button.addEventListener("click",function(){
+  
+// .value is whatever the user selects in the form  
  console.log ("Search Button Clicked", input.value)
-
+ getGeolocation(input.value);
 });
 
 <input name="searchTxt" type="text" maxlength="512" id="searchTxt" class="searchField"/>
